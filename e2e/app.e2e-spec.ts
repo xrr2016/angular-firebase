41a4fe62f7f4da9c79d0b9348535f581caa0f5fe
xrr2016 <xiaoranran1993@163.com>
2017-08-07 21:31:47 +0800

    :boom:

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { MyProjectPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('my-project App', () => {
+  let page: MyProjectPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new MyProjectPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
-
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
