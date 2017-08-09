f681fd419be15ed7a9c2d8a3d39bf320ed91fddb
xrr2016 <xiaoranran1993@163.com>
2017-08-09 21:59:16 +0800

    :zap:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import Hero from './class/hero'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>{{ title }}</h1>
+    <h2>My favorite hero is: {{ myHero.name }}</h2>
+    <p>Heros:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">{{ hero.name }}</li>
+    </ul>
+    <p *ngIf="heroes.length > 3">There are many heroes!</p>
+  `,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string
+  desc: string
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ]
+  myHero = this.heroes[0]
+
+  constructor() {
+    this.title = 'Tour of Heroes'
+    this.desc = 'Identity'
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
