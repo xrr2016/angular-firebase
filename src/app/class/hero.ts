1e5ead9bb71f889d1fc1701325e2b747e0cf4aa3
xrr2016 <xiaoranran1993@163.com>
2017-08-17 21:50:31 +0800

    :boom:

--- src/app/class/hero.ts
+++
@@ -1,6 +0,0 @@
-export default class Hero {
-  constructor (
-    public id: number,
-    public name: string
-  ) {}
-}
--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyC9lJnlctYWf-Ls_l29y1QY10Hn--CzhmY",
-  authDomain: "angular-firebase-72844.firebaseapp.com",
-  databaseURL: "https://angular-firebase-72844.firebaseio.com",
-  projectId: "angular-firebase-72844",
-  storageBucket: "angular-firebase-72844.appspot.com",
-  messagingSenderId: "21143885372"
-}
